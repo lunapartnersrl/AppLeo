fa71772c902c15f69d7fd8149ca9cecc0a86419d
Lorenzo Laricchia <lorenzo.laricchia@studio.unibo.it>
2020-02-04 09:23:20 +0100

    Added listacorsi section

---
+++ src/pages/listacorsi/listacorsi.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-listacorsi',
+  templateUrl: 'listacorsi.html'
+})
+export class ListaCorsi {
+
+  constructor(public navCtrl: NavController) {}
+
+}
