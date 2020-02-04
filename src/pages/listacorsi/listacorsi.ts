3e232089190a1dc4d055fb73588efbea8a9cc5a2
Lorenzo Laricchia <lorenzo.laricchia@studio.unibo.it>
2020-02-04 13:59:41 +0100

    Added functionalities to lista corsi section: 1.rendering a course list, 2.filter by date, 3.filter by ascendent title, 3.filter by searching partial title in the searchbar

--- src/pages/listacorsi/listacorsi.ts
+++ src/pages/listacorsi/listacorsi.ts
@@ -1,12 +1,68 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Corso } from '../../model/corso';
 
 @Component({
   selector: 'page-listacorsi',
-  templateUrl: 'listacorsi.html'
+  templateUrl: 'listacorsi.html',
 })
 export class ListaCorsi {
 
-  constructor(public navCtrl: NavController) {}
+
+  coursesList: Array<Corso>;
+  searchCourseValue: string = "";
+  customCoursesList: Array<Corso>;
+
+  constructor(public navCtrl: NavController) {
+    this.coursesList = [];
+    for(let i = 3; i > 0; i--){
+      this.coursesList.push(new Corso(
+        `Titolo ${i} `,
+        `2020-02-04T10:0${i}:35.652Z`,
+        `2020-02-05T10:0${i + 1}:35.652Z`,
+        'descrizione',
+        'red',
+        `via Indirizzo, ${i}`,
+        'prof.',
+        'Aula A2'
+      ));
+    }
+    this.customCoursesList = this.coursesList;
+    console.log(this.coursesList);
+  }
+
+  orderByDate(){
+    this.customCoursesList = this.coursesList;
+    this.customCoursesList.sort((a, b) =>{
+        let dateA = new Date(a.getISOStartDate());
+        let dateB = new Date(b.getISOStartDate());
+        if(dateA < dateB) return -1;
+        if(dateB > dateB) return 1;
+        return 0;
+    });
+    console.log(this.customCoursesList);
+  }
+
+  orderByTitle(){
+    this.customCoursesList = this.coursesList;
+    this.customCoursesList.sort((a, b) =>{
+      if(a.getTitle() < b.getTitle()) return -1;
+      if(a.getTitle() > b.getTitle()) return 1;
+      return 0;
+    });
+    console.log(this.customCoursesList);
+  }
+
+  onSearchInputChange(){
+    let tmp = [];
+    for(let elem of this.coursesList){
+      let title = elem.getTitle();
+      if(title.includes(this.searchCourseValue)){
+        tmp.push(elem);
+      }
+    }
+    console.log(this.customCoursesList);
+    this.customCoursesList = tmp;
+  }
 
 }
