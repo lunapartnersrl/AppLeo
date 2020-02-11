3427494210cca9e9ec81ef50a77678a295fbc210
Lorenzo Laricchia <lorenzo.laricchia@studio.unibo.it>
2020-02-11 12:37:03 +0100

    Added form validation and created data object for the email to send

--- src/pages/dettagliocorso/dettagliocorso.ts
+++ src/pages/dettagliocorso/dettagliocorso.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavParams, ViewController } from 'ionic-angular';
 import { Corso } from '../../model/corso';
-import { ListaCorsi } from '../listacorsi/listacorsi';
 
 @Component({
     selector: 'page-dettagliocorso',
@@ -11,6 +10,15 @@ import { ListaCorsi } from '../listacorsi/listacorsi';
 export class DettaglioCorso{
     corso: Corso = null;
 
+    inputValueName: string = "";
+    inputValueEmail: string = "";
+    inputValueTel: string = "";
+    inputValueMoreInfo: string = "";
+
+    warningName: string = "";
+    warningEmail: string = "";
+    warningTel: string = "";
+
     constructor(public viewCtrl: ViewController, public navParams: NavParams){
         console.log(navParams.data);
         this.corso = navParams.data;
@@ -18,5 +26,59 @@ export class DettaglioCorso{
 
     dismiss(){
         this.viewCtrl.dismiss();
+    }
+
+    formIsValid(): boolean{
+        console.log('Checking your form data...');
+
+        if(!this.inputValueName){
+            console.log('Please insert a name');
+            /* Add user output */
+            this.warningName = "Please insert a Name";
+            return false;
+        }
+        this.warningName = "";
+
+        let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!emailRegex.test(String(this.inputValueEmail).toLowerCase())){
+            console.log('wrong email format');
+            /* Add user output */
+            this.warningEmail = "Please insert a correct email";
+            return false;
+        }
+        this.warningEmail = "";
+
+        if(!this.inputValueTel){
+            console.log('Please insert your phone number');
+            /* Add user output */
+            this.warningTel = "Please insert a correct phone number";
+            return false;
+        }
+        this.warningTel = "";
+
+        console.log('...the form is valid');
+        return true;
+    }
+
+    submitForm(){
+        if(this.formIsValid()){
+
+            /* prepare data strucure for email sending */
+            let formData = {
+                subject: `Submitted form from course [${this.corso.courseId}] ${this.corso.courseTitle}`,
+                content: `
+                    Name: ${this.inputValueName} \n
+                    Email: ${this.inputValueEmail} \n
+                    Tel: ${this.inputValueTel} \n
+                    More information: ${this.inputValueMoreInfo} \n
+                `,
+                from: '',
+                to: ''
+            };
+
+            /* send information to server */
+            /* TO-DO */
+        }
+
     }
 }
